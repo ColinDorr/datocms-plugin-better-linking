53f69c614eee27f49c0002a4bf66a5d6597f68e0
Colin Dörr <colin.maloud@gmail.com>
2024-05-14 20:07:12 +0200

    Feat: added linkfield settings controls and additional rendering checks

--- src/components/LinkitEditor.tsx
+++ src/components/LinkitEditor.tsx
@@ -1,146 +1,163 @@
-import {
-    RenderFieldExtensionCtx,
-} from "datocms-plugin-sdk";
-import { 
-    Canvas, 
-    Form,
-    FieldGroup,
-    SelectField,
-    SwitchField,
-    TextField
-} from "datocms-react-ui";
+import React, { useState, MouseEvent } from 'react';
+import { RenderFieldExtensionCtx } from "datocms-plugin-sdk";
+import { Canvas, Form, FieldGroup, SelectField, SwitchField, TextField } from "datocms-react-ui";
 import Log from "./../utils/develop";
-
 import styles from "./styles.module.css";
 
 type PropTypes = {
   ctx: RenderFieldExtensionCtx;
 };
 
-export default function LinkitEditor({ ctx }: PropTypes) {
-    const ctxParameters = ctx.plugin.attributes.parameters as any;
-    const allowNewTarget = ctxParameters?.contentSettings?.allow_new_target || false;
+type LinkType = { label: string, value: string };
+type StylingType = { label: string, value: string };
+type ContentSettings = {
+    linkType: LinkType, 
+    stylingType: StylingType, 
+    link: string,
+    custom_text: string,
+    open_in_new_window: boolean
+ };
 
+export default function LinkitEditor({ ctx }: PropTypes) {
+    // Retrieve parameters from context
+    const ctxParameters = ctx.plugin.attributes.parameters as any;
+    console.log(ctxParameters)
+
+    // Function to get default value based on priority
+    const getDefaultValue = (key: string, fallback: any) => {
+        if(ctxParameters?.["field_settings"]?.[key] !== null){
+            return ctxParameters?.["field_settings"]?.[key]
+        } else if (ctxParameters?.["plugin_settings"]?.[key] !== null ){
+            return ctxParameters?.["plugin_settings"]?.[key]
+        } 
+        return fallback;
+    }
+
+    // Extract parameters with default values
+    const allowNewTarget = getDefaultValue("allow_new_target", false );
+    const allowCustomText = getDefaultValue("allow_custom_text", false );
+    const linkTypeOptions = getDefaultValue("linkTypeOptions", [] ) as LinkType[];
+    const stylingOptions = getDefaultValue("stylingOptions", [] ) as StylingType[];
+
+    // Default settings for content
+    const defaultSettings: ContentSettings = {
+        linkType: linkTypeOptions ? linkTypeOptions[0] : { label: "", value: "" },
+        stylingType: stylingOptions ? stylingOptions[0] : { label: "", value: "" },
+        link: getDefaultValue("link", ""),
+        custom_text: getDefaultValue("custom_text", ""),
+        open_in_new_window: getDefaultValue("open_in_new_window", false) 
+    }
+
+    // State to manage content settings
+    const [contentSettings, setContentSettings] = useState<ContentSettings>(defaultSettings);
+
+    // Function to update content settings
     const updateContentSettings = ( valueObject: object ) => {
+        setContentSettings({
+            ...contentSettings,
+            ...valueObject
+        })
         const updatedParameters = {
             ...ctxParameters,
             contentSettings: {
-                ...ctxParameters.contentSettings,
-                ...valueObject
+                ...contentSettings
             }
         };
         ctx.updatePluginParameters(updatedParameters);
-        Log(updatedParameters)
+        Log({updatedParameters})
     }
-
-    // Set target, when allow_target is false
-    if(!allowNewTarget){
-        const open_in_new_window_value = { ...ctxParameters?.contentSettings?.open_in_new_window || false };
-        updateContentSettings({"open_in_new_window": open_in_new_window_value});
-    }
-
-    // Set typeOptions array
-    const parameterMappings = {
-        allow_record: { label: "Record", value: "record" },
-        allow_url: { label: "URL", value: "url" },
-        allow_tel: { label: "Telephone number", value: "tel" },
-        allow_mail: { label: "Email address", value: "email" }
-    };
-    const typeOptions = Object.entries(parameterMappings)
-        .filter(([key]) => ctxParameters?.[key])
-        .map(([, value]) => value);
-
-    // Set stylingOptions Array
-    const stylingOptions = [
-        { label: "Default", value: "default" },
-        { label: "Primary", value: "primary" },
-        { label: "Secondary", value: "secondary" },
-        { label: "Tertiary", value: "tertiary" },
-        { label: "Quaternary", value: "quaternary" },
-    ]
 
     return (
         <Canvas ctx={ctx}>
-            <Form className={ styles.linkit } onSubmit={() => console.log("onSubmit")}>
-                <FieldGroup className={ styles.linkit__column }>
-                    <SelectField
-                        name="type"
-                        id="type"
-                        label="Type"
-                        value={ ctxParameters?.contentSettings?.type || typeOptions[0] }
-                        selectInputProps={{
-                            options: typeOptions,
-                        }}
-                        onChange={(newValue) => {
-                            updateContentSettings({ 
-                                "type": newValue, 
-                                "link": null 
-                            })
-                        }}
-                    />
-                    <SelectField
-                        name="styling"
-                        id="styling"
-                        label="Styling"
-                        value={ ctxParameters?.contentSettings?.styling || stylingOptions[0] }
-                        selectInputProps={{
-                        options: stylingOptions,
-                        }}
-                        onChange={(newValue) => {
-                            updateContentSettings({"styling": newValue})
-                        }}
-                    />
-                </FieldGroup>
-                <FieldGroup className={ styles.linkit__column }>
-                    {["url", "tel", "email"].includes(ctxParameters?.contentSettings?.type?.value || typeOptions[0].value) && (
-                        <TextField
-                            name="link"
-                            id="link"
-                            label={ctxParameters?.contentSettings?.type?.label || typeOptions[0].label}
-                            value={ctxParameters?.contentSettings?.link || ""}
-                            textInputProps={{ monospaced: true }}
+            {linkTypeOptions && linkTypeOptions.length > 0 ? (
+                <Form className={ styles.linkit } onSubmit={() => console.log("onSubmit")}>
+                    <FieldGroup className={ styles.linkit__column }>
+                        <SelectField
+                            name="type"
+                            id="type"
+                            label="Type"
+                            value={ contentSettings.linkType }
+                            selectInputProps={{
+                                options: linkTypeOptions as LinkType[],
+                            }}
                             onChange={(newValue) => {
-                                updateContentSettings({"link": newValue})
+                                updateContentSettings({"linkType": newValue})
                             }}
                         />
-                    )}
-                    {["record"].includes(ctxParameters?.contentSettings?.type?.value || typeOptions[0].value) && (
-                        <TextField
-                            name="link"
-                            id="link"
-                            label={ctxParameters?.contentSettings?.type?.label || typeOptions[0].label}
-                            value={ctxParameters?.contentSettings?.link || ""}
-                            textInputProps={{ monospaced: true }}
-                            onChange={(newValue) => {
-                                updateContentSettings({"link": newValue})
-                            }}
-                        />
-                    )}
-                    <TextField
-                        name="custom_text"
-                        id="custom_text"
-                        label="Custom text (Optional)"
-                        value={ ctxParameters?.contentSettings?.custom_text || null }
-                        textInputProps={{ monospaced: true }}
-                        onChange={(newValue) => {
-                            updateContentSettings({"custom_text": newValue})
-                        }}
-                    />
-                </FieldGroup>
-                <FieldGroup className={ styles.linkit__column_top }>
-                    { allowNewTarget ? (
-                        <SwitchField
-                            name="open_in_new_window"
-                            id="open_in_new_window"
-                            label="Open in new window"
-                            value={ctxParameters?.contentSettings?.open_in_new_window || false}
-                            onChange={(newValue) => {
-                                updateContentSettings({"open_in_new_window": newValue})
-                            }}
-                        />
-                    ) : null}
-                </FieldGroup>
-            </Form>
+                        {stylingOptions && stylingOptions.length > 0 && (
+                            <SelectField
+                                name="styling"
+                                id="styling"
+                                label="Styling"
+                                value={ contentSettings.stylingType }
+                                selectInputProps={{
+                                    options: stylingOptions as StylingType[],
+                                }}
+                                onChange={(newValue) => {
+                                    updateContentSettings({"stylingType": newValue})
+                                }}
+                            />
+                        )}
+                    </FieldGroup>
+                    <FieldGroup className={ styles.linkit__column }>
+                        {contentSettings.linkType && ["url", "tel", "email"].includes(contentSettings?.linkType?.value) ? (
+                            <TextField
+                                name="link"
+                                id="link"
+                                label={contentSettings.linkType.label}
+                                value={contentSettings.link}
+                                textInputProps={{ monospaced: true }}
+                                onChange={(newValue) => {
+                                    updateContentSettings({"link": newValue})
+                                }}
+                            />
+                        ) : (
+                            contentSettings.linkType && ["record"].includes(contentSettings?.linkType?.value) ? (
+                                <TextField
+                                    name="link"
+                                    id="link"
+                                    label={contentSettings.linkType.label}
+                                    value={contentSettings.link}
+                                    textInputProps={{ monospaced: true }}
+                                    onChange={(newValue) => {
+                                        updateContentSettings({"link": newValue})
+                                    }}
+                                />
+                            ) : null
+                        )}
+                        { allowCustomText && (
+                            <TextField
+                                name="custom_text"
+                                id="custom_text"
+                                label="Custom text (Optional)"
+                                value={ contentSettings.custom_text }
+                                textInputProps={{ monospaced: true }}
+                                onChange={(newValue) => {
+                                    updateContentSettings({"custom_text": newValue})
+                                }}
+                            />
+                        )}
+                    </FieldGroup>
+                    <FieldGroup className={ styles.linkit__column_top }>
+                        { allowNewTarget && (
+                            <SwitchField
+                                name="open_in_new_window"
+                                id="open_in_new_window"
+                                label="Open in new window"
+                                value={ contentSettings.open_in_new_window }
+                                onChange={(newValue) => {
+                                    contentSettings.open_in_new_window = newValue
+                                    updateContentSettings({"open_in_new_window": newValue})
+                                }}
+                            />
+                        )}
+                    </FieldGroup>
+                </Form>
+            ) : (
+                <div>
+                    <p><strong>Error!</strong><br/>No valid link types could be found for this field.<br/>Please add the wanted link types to the field appearence settings or the plugin settings</p>
+                </div>
+            )}
         </Canvas>
     );
 }
--- src/components/controlls/linkSetting.tsx
+++ src/components/controlls/linkSetting.tsx
@@ -1,47 +1,138 @@
-import { SwitchField} from "datocms-react-ui";
+import React, { useState } from 'react';
+import { Form, FieldGroup, Button, SwitchField } from "datocms-react-ui";
 import Log from "./../../utils/develop";
 
-const parameters = [
-    { label: "Allow Records", value: true, key: "allow_record" },
-    { label: "Allow URL", value: true, key: "allow_url" },
-    { label: "Allow Telephone numbers", value: true, key: "allow_tel" },
-    { label: "Allow Email addresses", value: true, key: "allow_email" },
-    { label: "Allow Target control", value: true, key: "allow_new_target" },
-];
+type PropTypes = {
+    ctx: any;
+    configType: string;
+};
 
-export default function linkSetting( { ctx, configType }: any ) {
+type ConfigSetting = { 
+    id: string, 
+    label: string; 
+    value: string | boolean; 
+};
+
+const LinkSetting: React.FC<PropTypes> = ({ ctx, configType }) => {
     const ctxParameters = ctx.plugin.attributes.parameters as any;
 
-    const updateCtx = (
-        valueObject: object
-    ) => {
-        const updatedParameters = {
+    const getDefaultValue = (key: string, fallback: string | boolean) => {
+        if(ctxParameters?.[configType]?.[key] !== null){
+            return ctxParameters?.[configType]?.[key]
+        } else if (ctxParameters?.["plugin_settings"]?.[key] !== null ){
+            return ctxParameters?.["plugin_settings"]?.[key]
+        } return {
+            fallback
+        }
+    }
+    
+    const parameters: ConfigSetting[] = [
+        { id: "allow_record",       label: "Allow Records",                 value: getDefaultValue("allow_record", true)        },
+        { id: "allow_url",          label: "Allow URL",                     value: getDefaultValue("allow_url", true)           },
+        { id: "allow_tel",          label: "Allow Telephone numbers",       value: getDefaultValue("allow_tel", true)           },
+        { id: "allow_email",        label: "Allow Email addresses",         value: getDefaultValue("allow_email", true)         },
+        { id: "allow_custom_text",  label: "Allow Custom text",             value: getDefaultValue("allow_custom_text", true)   },
+        { id: "allow_new_target",   label: "Allow Target control",          value: getDefaultValue("allow_new_target", true)    },
+    ];
+
+    const linkValues: any = {
+        allow_record: { label: "Record", value: "record" },
+        allow_url: { label: "URL", value: "url" },
+        allow_tel: { label: "Telephone number", value: "tel" },
+        allow_email: { label: "Email address", value: "email" }
+    };
+
+    const [configSettings, setConfigSettings] = useState<ConfigSetting[]>(parameters);
+
+    const updateCtx = () => {
+        // Generate list with setting booleans
+        const updatedConfigSettings = configSettings.reduce((
+            acc: { [label: string]: boolean }, 
+            obj: ConfigSetting
+        ) => {
+            acc[obj.id] = obj.value as boolean;
+            return acc;
+        }, {});
+
+        // Generate Array of linkTypeOptions (record, url, email, tel)
+        const linkTypeOptions: {label:string, value:string}[] = [];
+        Object.keys(updatedConfigSettings).forEach(id => {
+            if (linkValues[id]) {
+                linkTypeOptions.push({
+                    label: linkValues[id].label,
+                    value: linkValues[id].value
+                })
+            }
+        });
+
+        const newCtxParameters = {
             ...ctxParameters,
             [configType]: {
-              ...(ctxParameters?.[configType] || {}),
-             ...valueObject
+                ...(ctxParameters?.[configType] || {}),
+                ...updatedConfigSettings,
+                linkTypeOptions: linkTypeOptions
             }
-          };
+        }
 
-        ctx.updatePluginParameters(updatedParameters);
-        Log(updatedParameters);
+        ctx.updatePluginParameters(newCtxParameters);
+        Log({ newCtxParameters });
     };
 
     return (
         <div>
-            {parameters.map((param) => (
-                <div key={param.key}>
-                    <SwitchField
-                        id={param.key}
-                        name={param.key}
-                        label={param.label}
-                        value={ctxParameters?.[configType]?.[param.key] || ctxParameters?.["plugin_settings"]?.[param.key] || false}
-                        onChange={(newValue) => {
-                            updateCtx({[param.key]: newValue});
-                        }}
-                    />
-                </div>
-            ))}
+            <Form onSubmit={updateCtx}>
+                <FieldGroup>
+                    {configSettings.slice(0, -2).map((param: ConfigSetting) => (
+                        <div key={param.id}>
+                            <SwitchField
+                                id={param.id}
+                                name={param.id}
+                                label={param.label}
+                                value={param.value as boolean}
+                                onChange={(newValue) => {
+                                    setConfigSettings(prevSettings =>
+                                        prevSettings.map(setting => 
+                                            setting.id === param.id 
+                                            ? { ...setting, value: newValue } 
+                                            : setting
+                                        )
+                                    );
+                                }}
+                            />
+                        </div>
+                    ))}
+                </FieldGroup>
+                <br/>
+                <FieldGroup>
+                    {configSettings.slice(-2).map((param: ConfigSetting) => (
+                        <div key={param.id}>
+                            <SwitchField
+                                id={param.id}
+                                name={param.id}
+                                label={param.label}
+                                value={param.value as boolean}
+                                onChange={(newValue) => {
+                                    setConfigSettings(prevSettings =>
+                                        prevSettings.map(setting => 
+                                            setting.id === param.id 
+                                            ? { ...setting, value: newValue } 
+                                            : setting
+                                        )
+                                    );
+                                }}
+                            />
+                        </div>
+                    ))}
+                </FieldGroup>
+
+                <FieldGroup>
+                    <Button onClick={updateCtx} fullWidth buttonType="primary">
+                        Save link settings
+                    </Button>
+                </FieldGroup>
+            </Form>
         </div>
     );
 }
+
+export default LinkSetting;
